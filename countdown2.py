e7db1f1d28d2eafd30d58185789de031f939c3df
cascade <smccauley25@gmail.com>
2022-10-31 20:20:35 -0400

    countdown feature

---
+++ countdown2.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+
+# creates the countdown class
+def countdown(h, m, s):
+
+    # Calculates the total number of seconds
+    total_seconds = h * 3600 + m * 60 + s
+
+    # While loop that checks if total_seconds reaches zero
+    # If not zero, decrement total time by one second
+    while total_seconds > 0:
+        timer = datetime.timedelta(seconds = total_seconds)
+        print(timer, end="\r")
+        time.sleep(1)
+        total_seconds -= 1
+    print("Good, Bad, Im the guy with the keyboard.")
+
+h = input("Enter the time in hours: ")
+m = input("Enter the time in minutes: ")
+s = input("Enter the time in seconds: ")
+countdown(int(h), int(m), int(s))
